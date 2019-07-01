093fa976bf97731f7e8b296a8757ca207001b0e6
Alexandra <alexandrita@ukr.net>
2019-07-01 17:10:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-var age = prompt("Сколько лет?");
-age= +age;
+var age = +prompt("Сколько лет?");
 
-if (10<age && age< 14) {
+/*if (10<age && age< 14) {
 	alert(age + " лет");
-}
-	else if (age%10 == 2 || age%10 == 3 || age%10 == 4) {
+}*/
+if (!(10<age && age< 15 || 110<age && age< 115) && (age%10 == 2 || age%10 == 3 || age%10 == 4)) {
 		alert(age + " года");
 	} 
-	else if (age%10 == 1)
+	else if (age%10 == 1 && age!=11 && age!=111)
 				{
 					alert(age + " год");
 				} 
 
 					else {
 							alert(age + " лет");
-				         }
+}
